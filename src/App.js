5f9db850ea0cef4d75364b3c6aee46b5fa2f96a6
varshini <dreamvarshini@gmail.com>
2021-11-22 23:25:21 +0530

    ADDED: app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoReducer from './reducers/todoReducer'
+import './index.css';
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Provider store={ store }>
+        <Main ></Main>
+    </Provider>
     </div>
   );
 }
 
 export default App;
+
+
+
+ 
